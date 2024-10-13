60aadc4db4cbc12b5ef637f913071054823eebb0
andreyviana125 <andreyviana125@gmail.com>
2024-10-13 21:15:34 +0000

    angular-rebember-test

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgClass, NgFor } from '@angular/common';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      RouterOutlet,
+      NgFor,
+      NgClass
+    ]
+})
+export class AppComponent {
+
+  todoList: TodoItem[] = [];
+    newTask: string = '';
+    @ViewChild('todoText') todoInputRef: ElementRef<HTMLInputElement> = null!;
+
+    ngOnInit(): void {
+
+  }
+  addTask(text: string): void {
+    if (text.trim() !== '') {
+        const newTodoItem: TodoItem = {
+            id: Date.now(),
+            task: text.trim(),
+            completed: false
+        };
+        this.todoList.push(newTodoItem);
+        this.todoInputRef.nativeElement.value = '';
+    }
+}
+
+deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id);
+}
+
+toggleCompleted(id: number): void {
+    const todoItem = this.todoList.find(item => item.id === id);
+    if (todoItem) {
+        todoItem.completed = !todoItem.completed;
+    }
+}
+
+}
