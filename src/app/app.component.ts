75442e3b08cb28843c196544aceb30fb35cbee97
Andrey Viana Da Silva Araujo <asaraujo@tecnologiaunica.com.br>
2024-10-14 16:28:11 -0300

    adicionar botão "edit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgClass, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-export interface TodoItem {
+  export interface TodoItem {
   id: number;
   task: string;
   completed: boolean;
+  onEditMode: boolean;
 }
 
 
@@ -17,39 +19,59 @@ export interface TodoItem {
     imports: [
       RouterOutlet,
       NgFor,
-      NgClass
+      NgClass,
+      FormsModule
     ]
 })
 export class AppComponent {
 
   todoList: TodoItem[] = [];
-    newTask: string = '';
-    @ViewChild('todoText') todoInputRef: ElementRef<HTMLInputElement> = null!;
+  newTask: string = '';
+  @ViewChild('todoText') todoInputRef: ElementRef<HTMLInputElement> = null!;
 
-    ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
   addTask(text: string): void {
     if (text.trim() !== '') {
         const newTodoItem: TodoItem = {
             id: Date.now(),
             task: text.trim(),
-            completed: false
+            completed: false,
+            onEditMode: false
         };
         this.todoList.push(newTodoItem);
         this.todoInputRef.nativeElement.value = '';
     }
-}
+  }
 
-deleteTask(id: number): void {
+  editTask(id: number, text: string): void {
+    if (text.trim() !== '') {
+     const todoItemIndex: number = this.todoList.findIndex(item => item.id === id);
+     this.todoList[todoItemIndex].task = text;
+
+     this.toggleEditMode(id);
+     this.onEditNewTask('');
+    }
+  }
+
+  onEditNewTask(newTask: string) {
+    this.newTask = newTask;
+  }
+
+  deleteTask(id: number): void {
     this.todoList = this.todoList.filter(item => item.id !== id);
-}
+  }
 
-toggleCompleted(id: number): void {
+  toggleCompleted(id: number): void {
     const todoItem = this.todoList.find(item => item.id === id);
     if (todoItem) {
         todoItem.completed = !todoItem.completed;
     }
-}
+  }
+
+  toggleEditMode(id: number): void {
+    const todoItem = this.todoList.find(item => item.id === id);
+    if (todoItem) todoItem.onEditMode = !todoItem.onEditMode;
+  }
 
 }
